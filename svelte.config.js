f2b8dbb7e7fcd33d5a2b58c57ced64d79d8dce93
KrishnaSSH <krishna.pytech@gmail.com>
2026-08-29 16:01:02 +0530

    iterate

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({ runtime: 'nodejs20.x' })
+    adapter: adapter({ fallback: 'index.html' })
   }
 };
 
